df18af157ab5b2f1a5efdfea924fc968829900b3
Logant08 <logantillman@ymail.com>
2021-06-02 22:05:21 -0400

    WIP: Add template

--- hw2/ltillma4.hw2.py
+++ hw2/ltillma4.hw2.py
@@ -1,3 +1,45 @@
 # Author: Logan Tillman
 # NetID: Ltillma4
 # Hw2
+
+from numpy import zeros, ones, array, float64, inf
+from numpy import linalg
+from LUdecomp import *
+
+norm = linalg.norm  # Python allows you to rename functions.
+                    # Here, 'linalg.norm' is renamed to just 'norm'.
+TOL = 1
+err = 0
+n = 0
+while err < TOL:
+  n +=1
+  a = zeros((n,n),dtype=float64)
+  b = zeros((n),dtype=float64)
+  soln = ones((n), dtype=float64) # The correct solution is all 1's
+
+  # Use the loops below to define the matrix 'a' and vector 'b':
+  for i in range(n):
+    for j in range(n):
+
+      a[i,j] = a
+      b[i] = b
+
+  # Call appropriate functions from the LUdecomp.py module to
+  # solve the equations A x = b with the b-vector being overridden
+  # by the solution vector x.
+
+  a = 0 #FIXME dis hoe
+  b = 0 #FIXME dis hoe
+
+  #
+  # Your solution should be stored in 'b' (if you used a 
+  #  different variable name, modify the code below accordingly).
+  #
+  print("\n", n, " equations. ", "The solution is:\n", b)
+  err = norm(b-soln, ord=inf)
+  print("Error (inf-norm of difference)): ", err)
+
+print("^^^(Greater than TOL = ", TOL, ")^^^\n")
+print("********************************************\n")
+print("Max number of equations while error remains less than ", TOL, " is: ", n-1, "\n") 
+print("********************************************")
