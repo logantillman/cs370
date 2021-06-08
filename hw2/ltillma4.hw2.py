57da49051c4d0f40a9bfd3441a5d1af85edf18c9
logantillman <logantillman@ymail.com>
2021-06-08 18:14:22 -0400

    feat: Finish hw2

--- hw2/ltillma4.hw2.py
+++ hw2/ltillma4.hw2.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
-#####################################################################
-# problem2_1_15  (Problem Set 2.1, No. 15 on page 53 of textbook)
-# This problem requires the LUdecomp.py module (provided).
-#
-# Fill in the following template. Add the code that creates the
-# Matrix (a) and the vector (b). Then add the code that computes
-# the LU decomposition and produces a solution vector (see the
-# provided LUdecomp.py module for correct function calls to do that).
-####################################################################
+
+# Author: Logan Tillman
+# NetID: Ltillma4
+# Hw2
 
 from numpy import zeros, ones, array, float64, inf
 from numpy import linalg
 from LUdecomp import *
 
-norm = linalg.norm  # Python allows you to rename functions.                    
-                    # Here, 'linalg.norm' is renamed to just 'norm'.
+norm = linalg.norm
 
 TOL = 1.E-6
 err = 0
@@ -23,35 +17,21 @@ while err < TOL:
   n +=1
   a = zeros((n,n),dtype=float64)  
   b = zeros((n),dtype=float64)  
-  soln = ones((n), dtype=float64) # The correct solution is all 1's  
+  soln = ones((n), dtype=float64)
   
-  # Use the loops below to define the matrix 'a' and vector 'b':
   for i in range(n):    
     for j in range(n):      
-      a[i,j] = 1.0 / ((i + 1) + (j + 1) - 1)    
-      b[i] = sum(a[i])  # Call appropriate functions from the LUdecomp.py module to  
-
-    # print('a = ', a)
-    # print('b[i] = ', b[i])
-      
-    # solve the equations A x = b with the b-vector being overridden  
-    # by the solution vector x.  
+      a[i,j] = 1.0 / ((i + 1) + (j + 1) - 1)  # Creating the Hilbert Matrix
+      b[i] = sum(a[i])  # Calculating the b vector
     
-    a = LUdecomp(a)
-    b = LUsolve(a,b)
+  a = LUdecomp(a)
+  b = LUsolve(a,b)  # Solving the matrix and storing it in the b variable
 
-    #  
-    # Your solution should be stored in 'b' (if you used a   
-    #  different variable name, modify the code below accordingly).  
-    #  
-    print("\n", n, " equations. ", "The solution is:\n", b)
-    err = norm(b-soln, ord=inf)  
-    print("Error (inf-norm of difference)): ", err)
-    
-  print('Final A =', a)
-  print('Final B =', b)
+  print("\n", n, " equations. ", "The solution is:\n", b)
+  err = norm(b-soln, ord=inf)  
+  print("Error (inf-norm of difference)): ", err)
 
-  print("^^^(Greater than TOL = ", TOL, ")^^^\n")
-  print("********************************************\n")
-  print("Max number of equations while error remains less than ", TOL, " is: ", n-1, "\n")
-  print("********************************************")
+print("^^^(Greater than TOL = ", TOL, ")^^^\n")
+print("********************************************\n")
+print("Max number of equations while error remains less than ", TOL, " is: ", n-1, "\n")
+print("********************************************")
