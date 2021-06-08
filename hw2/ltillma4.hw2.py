e3c617ab1686377f35621f1b54ae280e9edee80b
logantillman <logantillman@ymail.com>
2021-06-07 23:23:25 -0400

    WIP: Made some progress

--- hw2/ltillma4.hw2.py
+++ hw2/ltillma4.hw2.py
@@ -1,45 +1,57 @@
-# Author: Logan Tillman
-# NetID: Ltillma4
-# Hw2
+#!/usr/bin/env python3
+#####################################################################
+# problem2_1_15  (Problem Set 2.1, No. 15 on page 53 of textbook)
+# This problem requires the LUdecomp.py module (provided).
+#
+# Fill in the following template. Add the code that creates the
+# Matrix (a) and the vector (b). Then add the code that computes
+# the LU decomposition and produces a solution vector (see the
+# provided LUdecomp.py module for correct function calls to do that).
+####################################################################
 
 from numpy import zeros, ones, array, float64, inf
 from numpy import linalg
 from LUdecomp import *
 
-norm = linalg.norm  # Python allows you to rename functions.
+norm = linalg.norm  # Python allows you to rename functions.                    
                     # Here, 'linalg.norm' is renamed to just 'norm'.
-TOL = 1
+
+TOL = 1.E-6
 err = 0
 n = 0
 while err < TOL:
   n +=1
-  a = zeros((n,n),dtype=float64)
-  b = zeros((n),dtype=float64)
-  soln = ones((n), dtype=float64) # The correct solution is all 1's
-
+  a = zeros((n,n),dtype=float64)  
+  b = zeros((n),dtype=float64)  
+  soln = ones((n), dtype=float64) # The correct solution is all 1's  
+  
   # Use the loops below to define the matrix 'a' and vector 'b':
-  for i in range(n):
-    for j in range(n):
+  for i in range(n):    
+    for j in range(n):      
+      a[i,j] = 1.0 / ((i + 1) + (j + 1) - 1)    
+      b[i] = sum(a[i])  # Call appropriate functions from the LUdecomp.py module to  
 
-      a[i,j] = a
-      b[i] = b
+    # print('a = ', a)
+    # print('b[i] = ', b[i])
+      
+    # solve the equations A x = b with the b-vector being overridden  
+    # by the solution vector x.  
+    
+    a = LUdecomp(a)
+    b = LUsolve(a,b)
 
-  # Call appropriate functions from the LUdecomp.py module to
-  # solve the equations A x = b with the b-vector being overridden
-  # by the solution vector x.
+    #  
+    # Your solution should be stored in 'b' (if you used a   
+    #  different variable name, modify the code below accordingly).  
+    #  
+    print("\n", n, " equations. ", "The solution is:\n", b)
+    err = norm(b-soln, ord=inf)  
+    print("Error (inf-norm of difference)): ", err)
+    
+  print('Final A =', a)
+  print('Final B =', b)
 
-  a = 0 #FIXME dis hoe
-  b = 0 #FIXME dis hoe
-
-  #
-  # Your solution should be stored in 'b' (if you used a 
-  #  different variable name, modify the code below accordingly).
-  #
-  print("\n", n, " equations. ", "The solution is:\n", b)
-  err = norm(b-soln, ord=inf)
-  print("Error (inf-norm of difference)): ", err)
-
-print("^^^(Greater than TOL = ", TOL, ")^^^\n")
-print("********************************************\n")
-print("Max number of equations while error remains less than ", TOL, " is: ", n-1, "\n") 
-print("********************************************")
+  print("^^^(Greater than TOL = ", TOL, ")^^^\n")
+  print("********************************************\n")
+  print("Max number of equations while error remains less than ", TOL, " is: ", n-1, "\n")
+  print("********************************************")
