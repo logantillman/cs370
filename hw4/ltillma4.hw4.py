a7f23db7c2be395a9bd633e7fff74c4b2077909f
logantillman <logantillman@ymail.com>
2021-06-15 19:34:29 -0400

    feat: Add comments

--- hw4/ltillma4.hw4.py
+++ hw4/ltillma4.hw4.py
@@ -1,32 +1,16 @@
 #!/usr/bin/env python3
 
-# prob2-3-19 (hwk #4)
-#
-#  Problem 2.3.19 (pg. 101)
-#
-#  1) Implement the Ax(v) function below so that it will initialize
-#     and return the matrix-vector product Ax, where A is given at
-#     the top of page 101 in the textbook.
-#  
-#  2) Use the provided conjGrad.py module to solve the system described
-#     in problem #19 (uncomment and fill in the '??' lines and provide
-#     the requested loop and final error difference).
-#
-# The correct solution is:
-#
-# [ 21.42857143 38.39285714  57.14285714   47.32142857   75.
-#   90.17857143 92.85714286 124.10714286 128.57142857]
-#
-########################################################################
+# Author: Logan Tillman
+# NetID: Ltillma4 
+# Hw4
 
 from numpy import zeros,array,linalg
 from conjGrad import *
 
-# Refer to pg. 101 for what the matrix Ax should be
-
 def Ax(v):
   Ax = zeros((9))*1.0
 
+  # Defining the matrix A
   A = zeros((9,9))*1.0
   A[0] = [-4, 1, 0, 1, 0, 0, 0, 0, 0]
   A[1] = [1, -4, 1, 0, 1, 0, 0, 0, 0]
@@ -38,6 +22,7 @@ def Ax(v):
   A[7] = [0, 0, 0, 0, 1, 0, 1, -4, 1]
   A[8] = [0, 0, 0, 0, 0, 1, 0, 1, -4]
 
+  # Calculating the matrix-vector product, only looking at the non-zero numbers in A
   for i in range(len(A)):
     sum = 0
     for j in range(len(A[i])):
@@ -51,25 +36,27 @@ def Ax(v):
 b = array([0,0,100,0,0,100,200,200,300])*(-1.0)
 x = zeros((9))*1.0
 tol = 1e-06
+
+# Calling conjGrad, giving it the matrix-vector product function
 s1,numIter = conjGrad(Ax, x, b, tol)
 print("\nThe solution is:\n",s1)
 print("\nNumber of iterations =",numIter, "using Tol: ", 1e-06)
 
 print("\n CG Convergence Test")
 print("Iterations   Tolerance")
-#
-# Create a loop here to call conjGrad with these tolerances and obtain/print the iteration
-# count with each tolerance used:  1.e-02, 1.e-04, 1.e-06, 1.e-08, 1.e-10, 1.e-12, 1.e-14, 1.e-16
-# tolerances = [1.e-06]
+
 tolerances = [1.e-02, 1.e-04, 1.e-06, 1.e-08, 1.e-10, 1.e-12, 1.e-14, 1.e-16]
+
+# Printing the number of iterations for each tolerance
 for tolerance in tolerances:
   sol, numIt = conjGrad(Ax, x, b, tolerance)
   print('    ', numIt, '       ', tolerance)
 
-# Finally, print the error between the solutions using the 1.e-06 and 1.e-16 tolerances
-
+# Calculating and printing the error? maybe?
 sol1 = conjGrad(Ax, x, b, 1.e-06)
 sol2 = conjGrad(Ax, x, b, 1.e-16)
+err = 0
+for i, sol in enumerate(sol1[0]):
+  err += abs(sol1[0][i] - sol2[0][i])
 
-#FIXME Complete this last part
-print("\nError between vectors obtained with tol=1e-06 and tol=1e-16: ", sol1 - sol2,"\n")
+print("\nError between vectors obtained with tol=1e-06 and tol=1e-16: ", err, "\n")
