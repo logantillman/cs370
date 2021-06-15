07d0e4733830a8863553e47c2fbb5966d3af6ffe
logantillman <logantillman@ymail.com>
2021-06-14 21:18:24 -0400

    feat: Almost finish hw4

--- hw4/ltillma4.hw4.py
+++ hw4/ltillma4.hw4.py
@@ -26,14 +26,32 @@ from conjGrad import *
 
 def Ax(v):
   Ax = zeros((9))*1.0
-  
-  # fill in the 9 rows of Ax
+
+  A = zeros((9,9))*1.0
+  A[0] = [-4, 1, 0, 1, 0, 0, 0, 0, 0]
+  A[1] = [1, -4, 1, 0, 1, 0, 0, 0, 0]
+  A[2] = [0, 1, -4, 0, 0, 1, 0, 0, 0]
+  A[3] = [1, 0, 0, -4, 1, 0, 1, 0, 0]
+  A[4] = [0, 1, 0, 1, -4, 1, 0, 1, 0]
+  A[5] = [0, 0, 1, 0, 1, -4, 0, 0, 1]
+  A[6] = [0, 0, 0, 1, 0, 0, -4, 1, 0]
+  A[7] = [0, 0, 0, 0, 1, 0, 1, -4, 1]
+  A[8] = [0, 0, 0, 0, 0, 1, 0, 1, -4]
+
+  for i in range(len(A)):
+    sum = 0
+    for j in range(len(A[i])):
+      if A[i, j] == 0:
+        continue
+      sum += (A[i, j] * v[j])
+    Ax[i] = sum
+
   return Ax
 
 b = array([0,0,100,0,0,100,200,200,300])*(-1.0)
 x = zeros((9))*1.0
 tol = 1e-06
-#s1,numIter = conjGrad(??)
+s1,numIter = conjGrad(Ax, x, b, tol)
 print("\nThe solution is:\n",s1)
 print("\nNumber of iterations =",numIter, "using Tol: ", 1e-06)
 
@@ -42,7 +60,14 @@ print("Iterations   Tolerance")
 #
 # Create a loop here to call conjGrad with these tolerances and obtain/print the iteration
 # count with each tolerance used:  1.e-02, 1.e-04, 1.e-06, 1.e-08, 1.e-10, 1.e-12, 1.e-14, 1.e-16
-#
-#
+# tolerances = [1.e-06]
+tolerances = [1.e-02, 1.e-04, 1.e-06, 1.e-08, 1.e-10, 1.e-12, 1.e-14, 1.e-16]
+for tolerance in tolerances:
+  sol, numIt = conjGrad(Ax, x, b, tolerance)
+  print('    ', numIt, '       ', tolerance)
+
 # Finally, print the error between the solutions using the 1.e-06 and 1.e-16 tolerances
-print("\nError between vectors obtained with tol=1e-06 and tol=1e-16: ",??????????,"\n")
+
+sol1 = conjGrad(Ax, x, b, 1.e-06)
+sol2 = conjGrad(Ax, x, b, 1.e-16)
+print("\nError between vectors obtained with tol=1e-06 and tol=1e-16: ", sol1 - sol2,"\n")
