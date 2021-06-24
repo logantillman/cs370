54cd5f759a253b83c08e1013b7e969d9187895bf
logantillman <logantillman@ymail.com>
2021-06-23 22:37:59 -0400

    feat: Finish hw8

--- hw8/ltillma4.hw8.py
+++ hw8/ltillma4.hw8.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
-# Template for COSC 370 Hwk #8
-# Approximation of derviatives using FDA, Richardson extrapolation,
-# and polynomial interpolation
-#
+
+# Author: Logan Tillman
+# NetID: Ltillma4
+# Hw8
+
 from numpy import round, array, polyfit, polyder, polyval
-#
+
 #  Problem 5.1.9
-#
-#  Fill in this comment block with a description of the approximation
-#  problem and method used.  Include your name and assignment number
-#  as well.
-#
-#  ???
-#  ???
-#  ???
-#  ???
-#  ???
-#
+
+#  For this problem, I'm computing an approximation using both a second-order accurate 
+#  central FDA method and Richardson Extrapolation.
+#  
+#  Name: Logan Tillman 
+#  Hw8
+
 print("Problem 5.1.9\n=============")
 #
 # This is an example of a dictionary with key:value pairs,
@@ -30,7 +27,7 @@ data9 = { 0.0: 0.0, 0.1: 0.078348, 0.2: 0.138910, 0.3: 0.192916, 0.4: 0.244981 }
 #
 h1 = 0.2
 x = 0.2
-centralApprox1 = ???
+centralApprox1 = (-data9[x-h1] + data9[x+h1]) / (2*h1)
 print("Central FDA Approximation (h = 0.2): %f" % centralApprox1)
 #
 # Now, half the stepsize and set h=0.1
@@ -38,25 +35,20 @@ print("Central FDA Approximation (h = 0.2): %f" % centralApprox1)
 # insure the proper key from the data9 dictionary will be selected.
 #
 h2 = h1 / 2.0
-centralApprox2 = ???
+centralApprox2 = (-data9[round(x-h2, decimals=1)] + data9[round(x+h2, decimals=1)]) / (2*h2)
 #
 # Now, apply Richardson Extrapolation to improve the approximation of f'(0.2)
 # using centralApprox1 and centralApprox2
 p = 2.0
-richardsonExtrap = ???
+richardsonExtrap = (2**p * centralApprox2 - centralApprox1) / (2**p - 1)
 print("Richardson Extrapolation (h2 = h1 / 2, p = 2): %f" % richardsonExtrap)
 #
 #  Problem 5.1.11
-#
-#  Fill in this comment block with a description of the approximation
-#  problem and method used.  
-#
-#  ???
-#  ???
-#  ???
-#  ???
-#  ???
-#
+
+#  For this problem I'm using polynomial interpolation to compute the derivatives at a specific point, x
+#  I'm using a polynomial of the third degree to approximate the function
+#  
+
 print("\nProblem 5.1.11\n==============")
 #
 # Here are the interpolation data points...
@@ -69,13 +61,13 @@ yData = array([15.18, 10.962, 1.92, -2.04])
 # First compute the coefficients for a cubic interpolating
 # polynomial using polyfit
 
-p = ???
+p = polyfit(xData, yData, 3)
 #
 # Then, use those coefficients to construct the first (d1) and
 # second (d2) derivative functions via polyder...
 #
-d1 = ???  
-d2 = ???
+d1 = polyder(p, 1) 
+d2 = polyder(p, 2)
 #
 # Here are the coeffcients of the actual polynomial, whose derivatives
 # we are approximating.
@@ -85,15 +77,15 @@ pActual = array([1, -0.3, -8.56, 8.448])
 # Use those coefficients to construct the first (d1Actual) and
 # second (d2Acutal) derivative functions via polyder...
 
-d1Actual = ???
-d2Actual = ???
+d1Actual = polyder(pActual, 1)
+d2Actual = polyder(pActual, 2)
 
 # Now, evaluate the first derivative of your cubic interpolating polynomial
 # at x=0, and then do the same for the first derivative of the actual
 # polynomial whose coefficients are given in pActual.
 
-fd =  ???
-fdActual = ???
+fd = polyval(d1, 0) 
+fdActual = polyval(d1Actual, 0)
 
 print("Interpolated f'(0): %f" % fd)
 print("Actual f'(0): %f" % fdActual)
@@ -103,8 +95,8 @@ print("Error: %f\n" % (fdActual - fd))
 # at x=0, and then do the same for the second derivative of the actual
 # polynomial whose coefficients are given in pActual.
 
-sd =  ???
-sdActual =  ???
+sd =  polyval(d2, 0)
+sdActual =  polyval(d2Actual, 0)
 
 print("Interpolated f''(0): %f" % sd)
 print("Actual f''(0): %f" % sdActual)
